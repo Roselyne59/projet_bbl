68ae28f96adbec341fe1b6707254fc20ed82de87
Q3DER <apo-96-06@hotmail.com>
2024-05-31 12:03:19 +0200

    J'ai optimiser les 2 classes en supprimant les codes redondant. j'ai ajouter le bouton pour creer un admin mais pas encore fait la partie back

--- login.py
+++ login.py
@@ -4,7 +4,43 @@ import json
 import os
 import tkinter as tk
 from tkinter import ttk
-from register import Register
+
+
+class Register:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Register")
+        self.frame = ttk.Frame(self.root)
+        self.frame = ttk.Frame(self.root, padding="450 250 450 450")
+        self.frame.pack(fill=tk.BOTH, expand=True)
+
+        # Initialiser les variables pour chaque champ
+        labels = ["First Name:", "Last Name:", "Birthdate:", "Email:", "Address:", "Login:", "Password:"]
+        self.variables = [tk.StringVar() for _ in labels]
+
+        # Créer les widgets pour chaque champ
+        for i, (label, var) in enumerate(zip(labels, self.variables)):
+            ttk.Label(self.frame, text=label).grid(column=0, row=i, sticky=tk.W)
+            ttk.Entry(self.frame, width=25, textvariable=var).grid(column=1, row=i, sticky=(tk.W, tk.E))
+
+        # Bouton pour s'enregistrer
+        ttk.Button(self.frame, text="Register", command=self.register_new_user).grid(column=1, row=len(labels),
+                                                                                     sticky=tk.E)
+
+        # Ajouter un peu d'espacement entre les widgets
+        for child in self.frame.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+
+    def register_new_user(self):
+        # Affichage des informations collectées pour exemple
+        user_info = ["First Name", "Last Name", "Birthdate", "Email", "Address", "Login", "Password"]
+        for info, var in zip(user_info, self.variables):
+            print(f"{info}: {var.get()}")
+
+        for widget in self.frame.winfo_children():
+            widget.destroy()
+        self.frame.destroy()
+        Login(self.root)
 
 
 class Login:
@@ -15,7 +51,7 @@ class Login:
         self.initialize_login_widgets()
 
     def initialize_login_widgets(self):
-        self.frame = ttk.Frame(self.root, padding="450 450 450 450")
+        self.frame = ttk.Frame(self.root, padding="450 250 450 450")
         self.frame.pack(fill=tk.BOTH, expand=True)
         self.login = tk.StringVar()
         self.password = tk.StringVar()
@@ -25,8 +61,11 @@ class Login:
         ttk.Entry(self.frame, width=25, textvariable=self.login).grid(column=1, row=0, sticky=(tk.W, tk.E))
         ttk.Label(self.frame, text="Password:").grid(column=0, row=1, sticky=tk.W)
         ttk.Entry(self.frame, width=25, textvariable=self.password).grid(column=1, row=1, sticky=(tk.W, tk.E))
-        ttk.Button(self.frame, text="Sign in", command=self.submit).grid(column=1, row=2, sticky=tk.E)
-        ttk.Button(self.frame, text="Register", command=self.load_register_form).grid(column=0, row=2, sticky=tk.W)
+        ttk.Button(self.frame, text="Sign in", command=self.submit).grid(column=2, row=2, sticky=(tk.W, tk.E))
+        ttk.Button(self.frame, text="Register a new user", command=self.load_register_form).grid(column=1, row=2,
+                                                                                                 sticky=(tk.W, tk.E))
+        ttk.Button(self.frame, text="Register a new admin", command=self.load_register_form).grid(column=0, row=2,
+                                                                                                  sticky=(tk.W, tk.E))
 
         # Espacement
         for child in self.frame.winfo_children():
@@ -40,30 +79,11 @@ class Login:
         # Effacer les widgets actuels
         for widget in self.frame.winfo_children():
             widget.destroy()
+        self.frame.destroy()
 
-        # Réinitialiser le titre de la fenêtre
-        self.root.title("Register")
+        # Créer une instance de Register
+        Register(self.root)
 
-        # Widgets pour l'enregistrement
-        labels = ["First Name:", "Last Name:", "Birthdate:", "Email:", "Address:", "Login:", "Password:"]
-        variables = [tk.StringVar() for _ in labels]  # Crée une nouvelle variable pour chaque champ
-
-        for i, (label, var) in enumerate(zip(labels, variables)):
-            ttk.Label(self.frame, text=label).grid(column=0, row=i, sticky=tk.W)
-            ttk.Entry(self.frame, width=25, textvariable=var).grid(column=1, row=i, sticky=(tk.W, tk.E))
-
-        # Bouton pour s'enregistrer
-        ttk.Button(self.frame, text="Register", command=lambda: self.register_new_user(variables)).grid(column=1, row=len(labels), sticky=tk.E)
-
-    def register_new_user(self, variables):
-        # Affichage des informations collectées pour exemple
-        user_info = ["First Name", "Last Name", "Birthdate", "Email", "Address", "Login", "Password"]
-        for info, var in zip(user_info, variables):
-            print(f"{info}: {var.get()}")
-
-        # Effacer les champs après l'enregistrement
-        for var in variables:
-            var.set("")
 
 if __name__ == "__main__":
     root = tk.Tk()
