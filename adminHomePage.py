e7b597aad62d87132b4b413fe22a6735f48549a5
Asloun <166741173+Asloun@users.noreply.github.com>
2024-06-09 15:00:46 +0200

    Update adminHomPage.py
    Adding buttons & functions

--- adminHomePage.py
+++ adminHomePage.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from memberHomePage import MemberHomePage
 from userApp import UserApp
-from book
+from bookApp import BookApp
+
 
 class AdminHomePage(MemberHomePage):
     def __init__(self, root):
@@ -9,15 +10,33 @@ class AdminHomePage(MemberHomePage):
         self.root.title("Espace Administrateur")
 
         self.admin_page_title()
+        self.admin_page_buttons()
 
     def admin_page_title(self):
-        title_label = tk.Label(self.root, text="Bibliothèque", font =('Helvetica', 24, "Bold"))
-        title_label.pack (pady=20)
+        title_label = tk.Label(self.root, text="Bibliothèque", font=("Helvetica", 26))
+        title_label.pack(pady=20)
 
     def admin_page_buttons(self):
         button_frame = tk.Frame(self.root)
-        button_frame.pack(expand = True)
+        button_frame.pack(expand= True)
 
-        Self.user_button = tk.Button(button_frame, text="Utilisateurs", command=)
+        self.user_button = tk.Button(button_frame, text="Utilisateurs", width=20, height=2, font=('Helvetica', 10, 'bold'), command= self.open_user_app)
+        self.user_button.grid(row=0, column=0, padx=20, pady=20)
+
+        self.user_button = tk.Button(button_frame, text="Livres", width=20, height=2, font=('Helvetica', 10, 'bold'), command= self.open_book_app)
+        self.user_button.grid(row=0, column=1, padx=20, pady=20)
+
+        self.user_button = tk.Button(button_frame, text="Emprunts", width=20, height=2, font=('Helvetica', 10, 'bold'), command= self.open_user_app)
+        self.user_button.grid(row=0, column=2, padx=20, pady=20)
+
+    def open_user_app(self):
+        user_window= tk.Tk()
+        UserApp(user_window)
+    
+    def open_book_app(self):
+        user_window= tk.Tk()
+        BookApp(user_window)
+        
+       
 
         
