e15e22701abe0c6cfddb969a7d0066482313b119
Afia <roselienrolo@gmail.com>
2024-06-10 00:52:11 +0200

    Update de la partie Book, fonctionalités correctes

--- book.py
+++ book.py
@@ -3,9 +3,6 @@ import json
 class Book:
     book_number = 1
 
-    with open('json/genres.json', 'r') as file:
-        all_genres = json.load(file)
-
     def __init__(self, book_id,title, authors, publication_year, isbn, editor, collections, genres):
         self.book_id = book_id
         self.title = title
@@ -14,16 +11,9 @@ class Book:
         self.isbn = isbn
         self.editor = editor
         self.collections = collections
-        self.genres = self.valid_genres(genres)
+        self.genres = genres
         if book_id >= Book.book_number:
             Book.book_number = book_id + 1
-
-    
-    def valid_genres(self, genres):
-        valid_genres = [genre for genre in genres if genre in self.all_genres]
-        if not valid_genres:
-            raise ValueError("Genre invalide. Les genres disponibles sont : " + ", ".join(self.all_genres))
-        return valid_genres
 
     def __str__(self):
         return f"{self.title} by {', '.join(self.authors)} ({self.publication_year})"
