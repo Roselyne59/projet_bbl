d6cd6c5791756252833c2dd55651706c347c543d
Asloun <166741173+Asloun@users.noreply.github.com>
2024-06-10 19:07:21 +0200

    update
    update last version

--- bookManager.py
+++ bookManager.py
@@ -2,45 +2,47 @@ import json
 import os
 from book import Book
 
-
 class BookManager:
     def __init__(self, json_file="json/books.json"):
         self.json_file = json_file
         self.books = self.load_books()
+        self.genres = self.load_genres()
 
     def load_books(self):
         if not os.path.exists(self.json_file):
             return []
-        with open(self.json_file, 'r') as file:
-            return [Book.from_dict(book) for book in json.load(file)]
+        with open(self.json_file, 'r', encoding='utf-8') as file:
+            books_data = json.load(file)
+            books = []
+            for book_data in books_data:
+                book = Book.from_dict(book_data)
+                books.append(book)
+            return books
 
     def save_books(self):
-        with open(self.json_file, 'w') as file:
-            json.dump([book.to_dict() for book in self.books], file, indent=4)
-
-    def load_genres(self):
-        json_file = os.path.join(os.path.dirname(__file__), 'json', 'genres.json')
-        with open(json_file, 'r') as file :
-            self.genres_data = json_file((file))
+        with open(self.json_file, 'w', encoding='utf-8') as file:
+            books_data = [book.to_dict() for book in self.books]
+            json.dump(books_data, file, ensure_ascii=False, indent=4)
 
     def add_book(self, book):
         self.books.append(book)
         self.save_books()
 
-    def remove_book(self, book_id):
-        self.books = [b for b in self.books if b.book_id != book_id]
-        self.save_books()
-
-    def update_book(self, edit_book):
-        for i, book in enumerate(self.books):
-            if book.book_id == edit_book.book_id:
-                self.books[i] = edit_book
+    def update_book(self, book):
+        for i, b in enumerate(self.books):
+            if b.book_id == book.book_id:
+                self.books[i] = book
                 self.save_books()
                 return
+        raise ValueError("Book not found")
 
-#    def find_book_by_title(self, title):
-#        return next((book for book in self.books if book.title == title), None)
+    def remove_book(self, book_id):
+        self.books = [book for book in self.books if book.book_id != book_id]
+        self.save_books()
 
-    
+    def load_genres(self):
+        with open('json/genres.json', 'r', encoding='utf-8') as file:
+            return json.load(file)
+
 
     
