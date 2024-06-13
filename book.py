da7c10770ffefb7c73a689f0882d943aee68074a
Afia <roselienrolo@gmail.com>
2024-06-13 11:12:35 +0200

    ajout is_available

--- book.py
+++ book.py
@@ -3,7 +3,7 @@ import json
 class Book:
     book_number = 1
 
-    def __init__(self, book_id,title, authors, publication_year, isbn, editor, collections, genres):
+    def __init__(self, book_id,title, authors, publication_year, isbn, editor, collections, genres, is_available = True):
         self.book_id = book_id
         self.title = title
         self.authors = authors
@@ -12,6 +12,7 @@ class Book:
         self.editor = editor
         self.collections = collections
         self.genres = genres
+        self.is_available = is_available
         if book_id >= Book.book_number:
             Book.book_number = book_id + 1
 
@@ -30,7 +31,8 @@ class Book:
             "isbn" : self.isbn,
             "éditeur" : self.editor,
             "collection" : self.collections,
-            "genres" : self.genres
+            "genres" : self.genres,
+            "is_available" : self.is_available
         }
 
     @staticmethod
@@ -43,5 +45,6 @@ class Book:
             data["isbn"],
             data["éditeur"],
             data["collection"],
-            data["genres"]
+            data["genres"],
+            data["is_available"]
         )
