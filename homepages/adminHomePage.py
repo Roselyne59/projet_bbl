1d53d1993b53a965d7827a0fa84c68d23ab45f15
Afia <roselienrolo@gmail.com>
2024-06-15 16:23:33 +0200

    documentation homepages

--- homepages/adminHomePage.py
+++ homepages/adminHomePage.py
@@ -7,7 +7,19 @@ from apps.borrowApp import BorrowApp
 from apps.viewAllReservationsApp import ViewAllReservationsApp
 
 class AdminHomePage(UserHomePage):
+    """_summary_
+
+    Args:
+        UserHomePage (_type_): _description_
+    """
     def __init__(self, root, firstname, lastname):
+        """_summary_
+
+        Args:
+            root (_type_): _description_
+            firstname (_type_): _description_
+            lastname (_type_): _description_
+        """
         super().__init__(root, firstname, lastname)
         self.root.title("Espace Administrateur")
 
@@ -15,10 +27,14 @@ class AdminHomePage(UserHomePage):
         self.admin_page_buttons()
 
     def admin_page_title(self):
+        """_summary_
+        """
         title_label = tk.Label(self.root, text="Bibliothèque", font=("Helvetica", 26))
         title_label.pack(pady=20)
 
     def admin_page_buttons(self):
+        """_summary_
+        """
         button_frame = tk.Frame(self.root)
         button_frame.pack(expand=True)
 
@@ -38,22 +54,32 @@ class AdminHomePage(UserHomePage):
         self.borrow_button.grid(row=0, column=4, padx=20, pady=20)
 
     def open_user_app(self):
+        """_summary_
+        """
         user_window = tk.Tk()
         UserApp(user_window)
 
     def open_book_app(self):
+        """_summary_
+        """
         book_window = tk.Tk()
         BookApp(book_window)
 
     def open_shelf_app(self):
+        """_summary_
+        """
         shelf_window = tk.Tk()
         ShelfApp(shelf_window)
 
     def open_borrow_app(self):
+        """_summary_
+        """
         borrow_window = tk.Tk()
         BorrowApp(borrow_window)
 
     def open_reservation_app(self):
+        """_summary_
+        """
         reservation_window = tk.Tk()
         ViewAllReservationsApp(reservation_window)
 
--- homepages/userHomePage.py
+++ homepages/userHomePage.py
@@ -1,7 +1,16 @@
 import tkinter as tk
 
 class UserHomePage:
+    """_summary_
+    """
     def __init__(self, root, firstname, lastname):
+        """_summary_
+
+        Args:
+            root (_type_): _description_
+            firstname (_type_): _description_
+            lastname (_type_): _description_
+        """
         self.root = root
         self.clear_screen()
         
@@ -9,19 +18,31 @@ class UserHomePage:
         self.logout_bouton()
 
     def clear_screen(self):
+        """_summary_
+        """
         for widget in self.root.winfo_children():
             widget.destroy()
     
     def welcome_message(self, firstname, lastname):
+        """_summary_
+
+        Args:
+            firstname (_type_): _description_
+            lastname (_type_): _description_
+        """
         welcome_message= f"Bienvenue {firstname} {lastname}"
         welcome_label = tk.Label(self.root, text = welcome_message, font=('helvetica', 16, 'bold'), fg='red')
         welcome_label.pack(pady=20)
     
     def logout_bouton(self):
+        """_summary_
+        """
         logout_button = tk.Button(self.root, text = "Se deconnecter", font=('Helvetica', 10, 'bold'), command=self.logout)
         logout_button.pack(pady=20)
 
     def logout(self):
+        """_summary_
+        """
         self.clear_screen()
         from models.login import LoginApp #Avoid cicular import between LoginApp and MemberHomePage
         LoginApp(self.root)
